7cbd01d9bb08021b20d00ce681b404152452234d
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-04-05 10:56:44 +0200

    switch project if already existent

--- setup-project.sh
+++ setup-project.sh
@@ -1,4 +1,4 @@
-oc new-project redmine
+oc new-project redmine || oc project redmine
 sudo oc login -u system:admin
 sudo oc adm policy add-scc-to-user anyuid -z default -n redmine
 oc new-app -f secret-template.yaml
